d3424e0de6fb7d2bff83af0f6c2d729ff5a8a3ef
Jan Redmann <janredmann1@gmail.com>
2021-06-29 11:38:41 +0200

    add relations

---
+++ library-with-relations/models/Book.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bookSchema = new Schema({
+	title: String,
+	description: String,
+	author: {
+		type: Schema.Types.ObjectId,
+		ref: 'Author' // that is the name of the model that this id refers to 
+	},
+	rating: Number,
+	reviews: [
+		{
+			user: String,
+			text: String
+		}
+	]
+});
+
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
